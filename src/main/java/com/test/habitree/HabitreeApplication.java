a4286784263ac73e1341854e1ce37b81e29dd3cc
ppjh94 <wleer157@gmail.com>
2025-11-18 18:12:55 +0900

    spring boot project 생성

---
+++ src/main/java/com/test/habitree/HabitreeApplication.java
@@ -0,0 +1,13 @@
+package com.test.habitree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/habitree/HabitreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.habitree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
